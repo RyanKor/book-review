22b0a683d64984db10d988c73dbe1859d3080c59
RyanKor <equus3144@gmail.com>
2024-10-29 07:42:13 +0900

    Finished: chapter 2

--- spark-the-definitive-guide/examples/stkim/02-spark-example/example.py
+++ spark-the-definitive-guide/examples/stkim/02-spark-example/example.py
@@ -11,14 +11,92 @@ spark = SparkSession.builder \
     .config(conf=conf) \
     .getOrCreate()
 
+myRange = spark.range(1000).toDF("number")
+
+
+# COMMAND ----------
+
+divisBy2 = myRange.where("number % 2 = 0")
+
+
+# COMMAND ----------
+
 flightData2015 = spark\
   .read\
   .option("inferSchema", "true")\
   .option("header", "true")\
-  .csv("/mnt/nvme/Workspace/book-review/spark-the-definitive-guide/examples/stkim/02-spark-example/2015-summary.csv")
+  .csv("/data/2015-summary.csv")
 
 # 데이터 미리보기
 flightData2015.show()
+
+# COMMAND ----------
+
+flightData2015.createOrReplaceTempView("flight_data_2015")
+
+
+# COMMAND ----------
+
+sqlWay = spark.sql("""
+SELECT DEST_COUNTRY_NAME, count(1)
+FROM flight_data_2015
+GROUP BY DEST_COUNTRY_NAME
+""")
+
+dataFrameWay = flightData2015\
+  .groupBy("DEST_COUNTRY_NAME")\
+  .count()
+
+sqlWay.explain()
+dataFrameWay.explain()
+
+
+# COMMAND ----------
+
+from pyspark.sql.functions import max
+
+flightData2015.select(max("count")).take(1)
+
+
+# COMMAND ----------
+
+maxSql = spark.sql("""
+SELECT DEST_COUNTRY_NAME, sum(count) as destination_total
+FROM flight_data_2015
+GROUP BY DEST_COUNTRY_NAME
+ORDER BY sum(count) DESC
+LIMIT 5
+""")
+
+maxSql.show()
+
+
+# COMMAND ----------
+
+from pyspark.sql.functions import desc
+
+flightData2015\
+  .groupBy("DEST_COUNTRY_NAME")\
+  .sum("count")\
+  .withColumnRenamed("sum(count)", "destination_total")\
+  .sort(desc("destination_total"))\
+  .limit(5)\
+  .show()
+
+
+# COMMAND ----------
+
+flightData2015\
+  .groupBy("DEST_COUNTRY_NAME")\
+  .sum("count")\
+  .withColumnRenamed("sum(count)", "destination_total")\
+  .sort(desc("destination_total"))\
+  .limit(5)\
+  .explain()
+
+
+# COMMAND ----------
+
 
 # SparkSession 종료
 spark.stop()
